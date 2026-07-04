d39e8aba7cde20752d120821de47604dcdf0c58d
pajetic <mssjimmy@gmail.com>
2026-07-03 22:21:42 -0400

    Level scene navigation + exit portal

---
+++ Assets/Scripts/ExitPortalController.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ExitPortalController : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        StartCoroutine(LoadNextLevel());
+    }
+
+    private IEnumerator LoadNextLevel()
+    {
+        yield return new WaitForSeconds(1f);
+        int nextSceneIndex = SceneManager.GetActiveScene().buildIndex + 1;
+        if (nextSceneIndex >= SceneManager.sceneCountInBuildSettings)
+        {
+            nextSceneIndex = 0;
+        }
+        SceneManager.LoadScene(nextSceneIndex);
+    }
+}
